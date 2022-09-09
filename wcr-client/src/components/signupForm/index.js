1765e5738b4d22a6d196f83241275f1ad07f680e
Your Name <you@example.com>
2022-09-09 11:37:37 +0100

    wip

--- wcr-client/src/components/signupForm/index.js
+++ wcr-client/src/components/signupForm/index.js
@@ -1,5 +1,56 @@
 import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
 
 export const SignupForm = () => {
-	return <div>signup</div>;
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	const onSubmit = (formData) => {
+		console.log('form data', formData);
+		console.log(formData.firstName);
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<input
+					placeholder='first name'
+					type='text'
+					{...register('firstName', { required: true })}
+				/>
+				<input
+					placeholder='last name'
+					type='text'
+					{...register('lastName', { required: true })}
+				/>
+				<input
+					placeholder='email'
+					type='email'
+					{...register('email', { required: true })}
+				/>
+				<input
+					placeholder='username'
+					type='text'
+					{...register('username', { required: true })}
+				/>
+				{errors.lastName && <p>Last name is required.</p>}
+				<input
+					placeholder='password'
+					type='password'
+					{...register('password', { required: true })}
+				/>
+				<input
+					placeholder='confirm password'
+					type='password'
+					{...register('confirmPassword', { required: true })}
+				/>
+				<button type='submit'>submit</button>
+			</form>
+		</div>
+	);
+
+	// todo: collect the rest
 };
