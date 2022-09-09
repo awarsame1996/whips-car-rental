47ae983e8f4af7718270588541f64a7b237d3a23
Your Name <you@example.com>
2022-09-09 12:45:36 +0100

    sign up form designed using bootstrap

--- wcr-client/src/components/signupForm/index.js
+++ wcr-client/src/components/signupForm/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Controller, useForm } from 'react-hook-form';
 
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 export const SignupForm = () => {
 	const {
 		register,
@@ -14,8 +17,58 @@ export const SignupForm = () => {
 	};
 
 	return (
-		<div>
-			<form onSubmit={handleSubmit(onSubmit)}>
+		<Form onSubmit={handleSubmit(onSubmit)}>
+			<Form.Group className='mb-3' controlId='firstName'>
+				<Form.Label>first name</Form.Label>
+				<Form.Control
+					type='text'
+					placeholder='Enter first Name'
+					{...register('firstName', { required: true })}
+				/>
+			</Form.Group>
+			<Form.Group className='mb-3' controlId='firstName'>
+				<Form.Label>last name</Form.Label>
+				<Form.Control
+					type='text'
+					placeholder='Enter last Name'
+					{...register('lastName', { required: true })}
+				/>
+			</Form.Group>
+			<Form.Group className='mb-3' controlId='firstName'>
+				<Form.Label>email</Form.Label>
+				<Form.Control
+					type='email'
+					placeholder='Enter email'
+					{...register('email', { required: true })}
+				/>
+			</Form.Group>
+			<Form.Group className='mb-3' controlId='firstName'>
+				<Form.Label>username</Form.Label>
+				<Form.Control
+					type='text'
+					placeholder='Enter username'
+					{...register('username', { required: true })}
+				/>
+			</Form.Group>
+			<Form.Group className='mb-3' controlId='formBasicPassword'>
+				<Form.Label>Password</Form.Label>
+				<Form.Control type='password' placeholder='Password' />
+			</Form.Group>
+			<Form.Group className='mb-3' controlId='formBasicPassword'>
+				<Form.Label>confirmPassword</Form.Label>
+				<Form.Control type='password' placeholder='Password' />
+			</Form.Group>
+			<Button variant='primary' type='submit'>
+				Submit
+			</Button>
+		</Form>
+	);
+
+	// todo: collect the rest
+};
+
+{
+	/* <form onSubmit={handleSubmit(onSubmit)}>
 				<input
 					placeholder='first name'
 					type='text'
@@ -48,9 +101,5 @@ export const SignupForm = () => {
 					{...register('confirmPassword', { required: true })}
 				/>
 				<button type='submit'>submit</button>
-			</form>
-		</div>
-	);
-
-	// todo: collect the rest
-};
+			</form> */
+}
