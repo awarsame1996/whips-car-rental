31cf7aef1d033c13bba89bd60749de025cdd7875
Your Name <you@example.com>
2022-09-13 18:29:57 +0100

    data mapped on car cards

--- wcr-client/src/components/vehiclesPage/vehiclesCards.js
+++ wcr-client/src/components/vehiclesPage/vehiclesCards.js
@@ -1,49 +1,46 @@
 import React from 'react';
 
 export const VehicleCard = (props) => {
-  const { car } = props;
+	const { car } = props;
 
-  const redirectToWhateverCarPage = () => {
-    // take you to the  specific car page
-    console.log('something');
-  };
+	const redirectToWhateverCarPage = () => {
+		// take you to the  specific car page
+		console.log('something');
+	};
 
-  return (
-    <div className="card  m-5 vehicle-card">
-      <div className="card-headr align-items-center">
-        {' '}
-        <h5 class="">
-          {car.make}, {car.model}
-        </h5>
-        <p className="card-text">
-          {car.seats} | {car.doors} | {car.transmission}
-        </p>
-      </div>
-      <img
-        src="https://media.istockphoto.com/photos/illustration-of-generic-hatchback-car-perspective-view-picture-id1150425295?b=1&k=20&m=1150425295&s=170667a&w=0&h=6RQC_JidCDrQYd_9JHFC5ANGp70KBg9nj_TpEDu-NZk="
-        class="card-img mt-3"
-        alt="Stony Beach"
-      />
+	return (
+		<div className='card  m-5 vehicle-card'>
+			<div className='card-headr align-items-center'>
+				{' '}
+				<h5 class=''>
+					{car.make}, {car.model}
+				</h5>
+				<p className='card-text'>
+					{car.specifications.seats} seats | {car.specifications.doors}{' '}
+					doors | {car.specifications.transmission}
+				</p>
+			</div>
+			<img src={car.imageUrl} class='card-img mt-3' alt='Stony Beach' />
 
-      <div className="mt-3 mb-2 card-descrption  align-items-center ">
-        <div className="pricing align-items-center">
-          <h5 className="card-title">Price: £{car.price}</h5>
-          <h5>
-            Discounted price: <s>£1600.00</s>
-          </h5>
-        </div>
-        <h7 className="mt-4">Brand:</h7>
-        <h7 className="mt-2">Deposit: £500</h7>
-        <h7 className="mt-2">Insurance: Comperhensive</h7>
-        <h7 className="mt-2">Incl. 450 miles</h7>
-      </div>
+			<div className='mt-3 mb-2 card-descrption  align-items-center '>
+				<div className='pricing align-items-center'>
+					<h5 className='card-title'>Price: £{car.price}</h5>
+					<h5>
+						Discounted price: <s>£1600.00</s>
+					</h5>
+				</div>
+				<h7 className='mt-4'>Brand:</h7>
+				<h7 className='mt-2'>Deposit: £500</h7>
+				<h7 className='mt-2'>Insurance: Comperhensive</h7>
+				<h7 className='mt-2'>Incl. 450 miles</h7>
+			</div>
 
-      <a
-        className="btn Vehicle-card-btn m-1"
-        onClick={redirectToWhateverCarPage}
-      >
-        Button
-      </a>
-    </div>
-  );
+			<a
+				className='btn Vehicle-card-btn m-1'
+				onClick={redirectToWhateverCarPage}
+			>
+				Button
+			</a>
+		</div>
+	);
 };
--- wcr-client/src/containers/vehiclesPage/index.js
+++ wcr-client/src/containers/vehiclesPage/index.js
@@ -8,56 +8,30 @@ import { Sorter } from '../../components/vehiclesPage/sorting';
 import { useQuery } from '@apollo/client';
 import { ALL_CARS } from '../../graphql/queries';
 
-export const VehiclesPage = async () => {
+export const VehiclesPage = () => {
 	// use
-	const Cars = () => {
-		const { loading, error, data } = useQuery(ALL_CARS);
+	const { loading, error, data } = useQuery(ALL_CARS);
 
-		if (loading) return 'Loading...';
+	if (loading) return 'Loading...';
 
-		if (error) return `Error! ${error.message}`;
-	};
+	if (error) return `Error! ${error.message}`;
 
-	const myArr = [
-		{
-			make: 'Ford',
-			model: 'Focus',
-			seats: 4,
-			doors: 3,
-			transmission: 'automatic',
-			price: 7000,
-		},
-		{
-			make: 'Lexus',
-			model: 'IS300',
-			seats: 5,
-			doors: 5,
-			transmission: 'automatic',
-			price: 12000,
-		},
-		{
-			make: 'Ford',
-			model: 'Focus',
-			seats: 4,
-			doors: 3,
-			transmission: 'automatic',
-			price: 7000,
-		},
-	];
-	// graph ql query to get car data
-
-	return (
-		<div>
-			{/* component one */}
-			<VehicleHero></VehicleHero>
-			<Sorter></Sorter>
-			<div className='vh-60 mt-1 d-flex justify-content-center flex-row flex-wrap'>
-				{myArr.map((car) => {
-					return <VehicleCard car={car}> </VehicleCard>;
-				})}
+	if (data) {
+		console.log(data);
+		return (
+			<div>
+				{/* component one */}
+				<VehicleHero></VehicleHero>
+				<Sorter></Sorter>
+				<div className='vh-60 mt-1 d-flex justify-content-center flex-row flex-wrap'>
+					{data.cars.map((car) => {
+						return <VehicleCard car={car}> </VehicleCard>;
+					})}
+				</div>
+				<div className='vh-40 border border-danger mt-1'></div>
+				<div className='vh-40 border border-danger mt-1'></div>
 			</div>
-			<div className='vh-40 border border-danger mt-1'></div>
-			<div className='vh-40 border border-danger mt-1'></div>
-		</div>
-	);
+		);
+	}
+	// graph ql query to get car data
 };
