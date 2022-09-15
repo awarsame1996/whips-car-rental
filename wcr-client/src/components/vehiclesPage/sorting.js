403a0fcf8306353738acd0912c33738eefce4510
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-09-15 20:41:36 +0100

    vehicle cards finalised

--- wcr-client/src/components/vehiclesPage/sorting.js
+++ wcr-client/src/components/vehiclesPage/sorting.js
@@ -5,93 +5,87 @@ import React from 'react';
 
 export const Sorter = () => {
   return (
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-12">
-          <div className="options-nav mb-5 ">
-            <div class="btn-group">
-              <button
-                type="button"
-                className="btn  dropdown-toggle"
-                data-mdb-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Any Brand
-              </button>
-              <ul className="dropdown-menu dropdown-menu-end">
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Action
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Another action
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Something else here
-                  </button>
-                </li>
-              </ul>
-            </div>
-            <div class="btn-group">
-              <button
-                type="button"
-                className="btn dropdown-toggle"
-                data-mdb-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Any Model
-              </button>
-              <ul className="dropdown-menu dropdown-menu-end">
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Action
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Another action
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Something else here
-                  </button>
-                </li>
-              </ul>
-            </div>
-            <div class="btn-group">
-              <button
-                type="button"
-                className="btn dropdown-toggle"
-                data-mdb-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Price High to Low
-              </button>
-              <ul className="dropdown-menu dropdown-menu-end">
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Action
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Another action
-                  </button>
-                </li>
-                <li>
-                  <button className="dropdown-item" type="button">
-                    Something else here
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+    <div className="options-nav mb-5 ">
+      <div class="btn-group ">
+        <button
+          type="button"
+          className="btn  btn-dark  dropdown-toggle"
+          data-mdb-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Any Brand
+        </button>
+        <ul className="dropdown-menu dropdown-menu-end">
+          <li>
+            <button className="dropdown-item" type="button">
+              Action
+            </button>
+          </li>
+          <li>
+            <button className="dropdown-item" type="button">
+              Another action
+            </button>
+          </li>
+          <li>
+            <button className="dropdown-item" type="button">
+              Something else here
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div class="btn-group">
+        <button
+          type="button"
+          className="btn  btn-dark dropdown-toggle"
+          data-mdb-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Any Model
+        </button>
+        <ul className="dropdown-menu dropdown-menu-end">
+          <li>
+            <button className="dropdown-item" type="button">
+              Action
+            </button>
+          </li>
+          <li>
+            <button className="dropdown-item" type="button">
+              Another action
+            </button>
+          </li>
+          <li>
+            <button className="dropdown-item" type="button">
+              Something else here
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div class="btn-group">
+        <button
+          type="button"
+          className="btn  btn-dark dropdown-toggle"
+          data-mdb-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Price High to Low
+        </button>
+        <ul className="dropdown-menu dropdown-menu-end">
+          <li>
+            <button className="dropdown-item" type="button">
+              Action
+            </button>
+          </li>
+          <li>
+            <button className="dropdown-item" type="button">
+              Another action
+            </button>
+          </li>
+          <li>
+            <button className="dropdown-item" type="button">
+              Something else here
+            </button>
+          </li>
+        </ul>
       </div>
     </div>
   );
