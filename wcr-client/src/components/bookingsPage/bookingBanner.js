0bdc3b195bd6a2de4e57733073a20a9ffb0bace3
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-09-16 20:49:37 +0100

    bookins page complete

---
+++ wcr-client/src/components/bookingsPage/bookingBanner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const BookingBanner = () => {
+  return (
+    <div className="d-flex justify-content-center ">
+      <div className="bg-image bookings-bg mb-5 ">
+        <div className="mask background-div">
+          <div className="d-flex justify-content-center align-items-center h-100"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
