2943c5bf131e041b7641ac394a853c842622afc5
Your Name <you@example.com>
2022-09-22 23:18:08 +0100

    fsd

--- wcr-client/src/components/neonButton/index.js
+++ wcr-client/src/components/neonButton/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './index.css';
 
 export const NeonButton = () => {
-  return (
-    <a className="neon-button mt-5 mb-5" href="#">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      View Our Catalogue Of Cars
-    </a>
-  );
+	return (
+		<a className='neon-button mt-5 mb-5' href='/vehicles'>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			View Our Catalogue Of Cars
+		</a>
+	);
 };
