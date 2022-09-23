00c8352eee7daf65dbd3b260f30042b79ecbb366
Your Name <you@example.com>
2022-09-23 18:27:02 +0100

    updated

--- server/index.js
+++ server/index.js
@@ -7,8 +7,21 @@ const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 const { authMiddleware } = require('./context/auth');
 const { InMemoryLRUCache } = require('@apollo/utils.keyvaluecache');
+const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 4000;
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const init = async () => {
 	try {
