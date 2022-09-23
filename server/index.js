afc49a62bb8c7c13e50fc1e69810d6358e06673c
Your Name <you@example.com>
2022-09-23 15:11:23 +0100

    update

--- server/index.js
+++ server/index.js
@@ -7,9 +7,19 @@ const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 const { authMiddleware } = require('./context/auth');
 const { InMemoryLRUCache } = require('@apollo/utils.keyvaluecache');
+const express = requir('express');
 
 const PORT = process.env.PORT || 4000;
 
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
 const init = async () => {
 	try {
 		await connectToDatabase();
