b393abd474be5b9b67b649132c07f8a4caa44cb9
Your Name <you@example.com>
2022-09-23 15:48:32 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ import 'aos/dist/aos.css';
 import { Footer } from './components/footer/footer';
 
 const httpLink = createHttpLink({
-	uri: 'env.REACT_APP_GRAPHQL || "http://localhost:4000",',
+	uri: env.REACT_APP_GRAPHQL || 'http://localhost:4000',
 	credentials: 'same-origin',
 });
 
